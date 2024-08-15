69c90ee5b5f98f5be94457af36ea05ad712161e5
alkrona <alkrona117@gmail.com>
2024-08-15 12:56:03 +1000

    inital commit

---
+++ temp.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "temp" (
+"id" INTEGER,
+"items" TEXT NOT NULL,
+"sales_amount" INTEGER NOT NULL,
+"sales" NUMERIC NOT NULL,
+"item_id" INT NOT NULL,
+"venue" TEXT NOT NULL,
+"year" TEXT NOT NULL,
+"period" TEXT NOT NULL,
+PRIMARY KEY("id")
+);
---
+++ temp2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "temp2" (
+"items" TEXT NOT NULL,
+"sales_amount" INTEGER NOT NULL,
+"sales" NUMERIC NOT NULL,
+"item_id" INT NOT NULL,
+"venue" TEXT NOT NULL,
+"year" TEXT NOT NULL,
+"period" TEXT NOT NULL
+);
